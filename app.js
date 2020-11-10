beb05d20575a691d275035fc4c37686ec126b2bd
tomoverflow <adeyemiadetomiwa@gmail.com>
2020-11-10 21:05:02 +0100

    Updating

--- app.js
+++ app.js
@@ -8,8 +8,20 @@ const app = express()
 app.listen(3000);
 
 app.get('/', (req, res)=>{
+    // res.send('<p> Home page</p>')
+    res.sendFile('./views/index.html', { root: __dirname})
+})
+app.get('/about', (req, res)=>{
+    res.sendFile('./views/about.html', { root: __dirname})
+})
 
-    res.send()
+//redirects
+app.get('/about-us', (req, res)=>{
+    res.redirect('/about');
+})
 
+//404 page
+app.use((req, res)=>{
+    res.status(404).res.sendFile('./views/404.html', { root: __dirname})
 
 })
