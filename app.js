f89521d7716eb314111dbe7d19c6016a89be6036
tomoverflow <adeyemiadetomiwa@gmail.com>
2020-11-11 22:02:24 +0100

    Express Js and Ejs

--- app.js
+++ app.js
@@ -3,25 +3,42 @@ const express = require('express')
 //express app
 const app = express()
 
+app.set('view engine', 'ejs');
+
 
 //listen
 app.listen(3000);
 
 app.get('/', (req, res)=>{
     // res.send('<p> Home page</p>')
-    res.sendFile('./views/index.html', { root: __dirname})
-})
-app.get('/about', (req, res)=>{
-    res.sendFile('./views/about.html', { root: __dirname})
+    // res.sendFile('./views/index.html', { root: __dirname})
+
+    const blogs = [
+        {title: 'Tom one', snippet: 'This is tom one'},
+        {title: 'Tom two', snippet: 'This is tom two'},
+        {title: 'Tom three', snippet: 'This is tom three'}
+    ]
+    res.render('index', { title: 'Home', blogs});
 })
 
-//redirects
-app.get('/about-us', (req, res)=>{
-    res.redirect('/about');
+app.get('/about', (req, res)=>{
+    // res.sendFile('./views/about.html', { root: __dirname})
+    res.render('about', { title: 'About'})
 })
+
+app.get('/blog/create', (req, res)=>{
+    // res.sendFile('./views/about.html', { root: __dirname})
+    res.render('create', { title: 'Create Blog'})
+})
+
+// //redirects
+// app.get('/about-us', (req, res)=>{
+//     res.redirect('/about');
+// })
 
 //404 page
 app.use((req, res)=>{
-    res.status(404).res.sendFile('./views/404.html', { root: __dirname})
+    // res.status(404).res.sendFile('./views/404.html', { root: __dirname})
+    res.status(404).res.render('404', { title: 'Error'})
 
 })
